1eaa7a982178908917d37f12ceb49194a8b8a698
Iain <iain@lifebeyondfife.com>
2015-07-07 22:15:23 +0100

    Add EntityFramework and SQLite packages.

---
+++ Cm93.State/Sqlite/Cm93Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cm93.State.Sqlite
+{
+    public class Cm93Context : DbContext
+    {
+    }
+}
