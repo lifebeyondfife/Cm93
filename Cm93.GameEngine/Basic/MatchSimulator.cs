1f033013a9b9d3d4ca049ef6d195e6fc2d56e21c
Iain <iain@lifebeyondfife.com>
2015-12-28 22:30:18 +0000

    Fix combined rating heatmap.

--- Cm93.GameEngine/Basic/MatchSimulator.cs
+++ Cm93.GameEngine/Basic/MatchSimulator.cs
@@ -571,15 +571,24 @@ namespace Cm93.GameEngine.Basic
 					var rating = (int) (TeamSkills.HomeTeamDribbling(new Coordinate { X = (double) i / width, Y = (double) j / height }) -
 						TeamSkills.AwayTeamDribbling(new Coordinate { X = (double) i / width, Y = (double) j / height }));
 
-					stringBuilder.Append(rating + " ");
+					stringBuilder.Append(rating);
 
-					j += (int) Math.Log10(rating) + 1;
+					j += (int) Math.Log10(Math.Abs(rating)) + 1;
 
-					if (rating < 1)
+					if (rating < 0)
 						++j;
 
-					if (j == height - 1)
+					if (j == height)
 						stringBuilder.Append("|");
+					else if (j == height - 1)
+						stringBuilder.Append(" |");
+					else if (j == height - 2)
+					{
+						stringBuilder.Append("  |");
+						j += 2;
+					}
+					else
+						stringBuilder.Append(" ");
 				}
 
 				stringBuilder.AppendLine();
